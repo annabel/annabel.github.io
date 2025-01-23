8c9000ec88e85ed5cd54064e2f3e7d5d020ce89c
Annabel Gray <annabel.dunstone@gmail.com>
2025-01-22 20:47:39 -0700

    Fix dark mode and copy to clipboard bugs; change dark mode color palette

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+// Dark mode toggle
 const toggleButton = document.getElementById("dark-mode-toggle");
 const body = document.body;
 
 if (localStorage.getItem("dark-mode") === "true") {
   body.classList.add("dark");
+  toggleButton.checked = true;
 } else {
   body.classList.remove("dark");
+  toggleButton.checked = false;
 }
 
 toggleButton.addEventListener("click", () => {
-  console.log("helo");
   body.classList.toggle("dark");
 
   if (body.classList.contains("dark")) {
     localStorage.setItem("dark-mode", "true");
+    toggleButton.checked = true;
   } else {
     localStorage.setItem("dark-mode", "false");
+    toggleButton.checked = false;
   }
 });
 
+// Copy to clipboard
 function copyToClipboard() {
   const email = document.getElementById("email").textContent;
+  navigator.clipboard.writeText(email);
 }
